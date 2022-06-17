68d35e383f478d72e81520788c5bef07b66e1798
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-18 07:10:47 +0800

    Create createElement.js

---
+++ react/createElement.js
@@ -0,0 +1,23 @@
+export function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map(child =>
+                typeof child === "object"
+                    ? child
+                    : createTextElement(child) // dfs创建子节点
+            ),
+        },
+    }
+}
+
+function createTextElement(text) {
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text,
+            children: [],
+        },
+    }
+}
